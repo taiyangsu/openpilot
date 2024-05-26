df1e1691efe6ef16c646b1ca2932d32c3e0cbe02
garrettpall <76917194+garrettpall@users.noreply.github.com>
2024-05-26 07:01:08 -0400

    + Mazda Stock Custom Longitudinal Control

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 from cereal import car, custom
 from openpilot.common.conversions import Conversions as CV
-from openpilot.selfdrive.car.mazda.values import CAR, LKAS_LIMITS
+from openpilot.selfdrive.car.mazda.values import CAR, LKAS_LIMITS, Buttons
 from openpilot.selfdrive.car import create_button_events, get_safety_config
 from openpilot.selfdrive.car.interfaces import CarInterfaceBase
+from openpilot.common.params import Params
+
+params = Params()
 
 ButtonType = car.CarState.ButtonEvent.Type
 EventName = car.CarEvent.EventName
 FrogPilotButtonType = custom.FrogPilotCarState.ButtonEvent.Type
+BUTTONS_DICT = {Buttons.SET_PLUS: ButtonType.accelCruise, Buttons.SET_MINUS: ButtonType.decelCruise,
+                Buttons.RESUME: ButtonType.resumeCruise, Buttons.CANCEL: ButtonType.cancel}
 
 class CarInterface(CarInterfaceBase):
 
@@ -29,6 +34,20 @@ class CarInterface(CarInterfaceBase):
 
     ret.centerToFront = ret.wheelbase * 0.41
 
+    if params.get_bool("CSLCEnabled"):
+        # Used for CEM with CSLC
+        ret.openpilotLongitudinalControl = True
+        ret.longitudinalTuning.deadzoneBP = [0.]
+        ret.longitudinalTuning.deadzoneV = [0.9]  # == 2 mph allowable delta
+        ret.stoppingDecelRate = 4.5  # == 10 mph/s
+        ret.longitudinalActuatorDelayLowerBound = 1.
+        ret.longitudinalActuatorDelayUpperBound = 2.
+
+        ret.longitudinalTuning.kpBP = [8.94, 7.2, 28.]  # 8.94 m/s == 20 mph
+        ret.longitudinalTuning.kpV = [0., 4., 2.]  # set lower end to 0 since we can't drive below that speed
+        ret.longitudinalTuning.kiBP = [0.]
+        ret.longitudinalTuning.kiV = [0.1]
+
     return ret
 
   # returns a car.CarState
@@ -37,6 +56,7 @@ class CarInterface(CarInterfaceBase):
 
      # TODO: add button types for inc and dec
     ret.buttonEvents = [
+      *create_button_events(self.CS.cruise_buttons, self.CS.prev_cruise_buttons, BUTTONS_DICT),
       *create_button_events(self.CS.distance_button, self.CS.prev_distance_button, {1: ButtonType.gapAdjustCruise}),
       *create_button_events(self.CS.lkas_enabled, self.CS.lkas_previously_enabled, {1: FrogPilotButtonType.lkas}),
     ]
