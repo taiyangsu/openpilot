b782ba9311e2c1516dab8909087732889d23d804
机械小鸽 <128568434+jixiexiaoge@users.noreply.github.com>
2025-03-27 08:37:13 +0800

    fix

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -17,7 +17,7 @@ BUTTONS_DICT = {Buttons.SET_PLUS: ButtonType.accelCruise, Buttons.SET_MINUS: But
 class CarInterface(CarInterfaceBase):
 
   @staticmethod
-  def _get_params(ret, candidate, fingerprint, car_fw, experimental_long, docs):
+  def _get_params(ret, params, candidate, fingerprint, car_fw, disable_openpilot_long, experimental_long, docs):
     ret.carName = "mazda"
     ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.mazda)]
     ret.radarUnavailable = True
